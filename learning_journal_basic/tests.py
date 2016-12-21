7049c85f2354c6c46b42f4746a987323c67ec733
Your Name <you@example.com>
2016-12-21 00:31:39 -0800

    tests.py

--- learning_journal_basic/tests.py
+++ learning_journal_basic/tests.py
@@ -1,3 +1,125 @@
+"""."""
+import pytest
+from pyramid import testing
+
+
+@pytest.fixture()
+def req():
+    """."""
+    the_request = testing.DummyRequest()
+    return the_request
+
+
+def test_home_page_renders_file_data(req):
+    """My home page view returns some data."""
+    from .views import home_page
+    response = home_page(req)
+    assert "bag_list" in response
+
+
+@pytest.fixture()
+
+
+@pytest.fixture()
+def testapp():
+    from webtest import TestApp
+    from learning_journal_basic import main
+    app = main({})
+    from webtest import TestApp
+    return TestApp(app)
+
+def test_home_page_has_list(testapp):
+    response = testapp.get("/", status=200)
+    inner_html = response.html
+
+
+# from webtest import TestApp
+
+# From learning_journal_basic import main
+
+# testapp = TestApp(min({}))
+
+# response = testapp.get("/", status=200)
+
+# from pprint import pprint
+
+# pprint(dir(response))
+
+# html = response.html
+
+# html.find_all("li")
+
+# len(html.find_all("li"))
+# >> 5
+
+# html.find("ul").child
+
+# <<ul = html.find('ul')
+# <<ul
+
+
+
+"""."""
+import pytest
+from pyramid import testing
+
+
+@pytest.fixture()
+def req():
+    """."""
+    the_request = testing.DummyRequest()
+    return the_request
+
+
+def test_home_page_renders_file_data(req):
+    """My home page view returns some data."""
+    from .views import home_page
+    response = home_page(req)
+    assert "bag_list" in response
+
+
+@pytest.fixture()
+
+
+@pytest.fixture()
+def testapp():
+    from webtest import TestApp
+    from learning_journal_basic import main
+    app = main({})
+    from webtest import TestApp
+    return TestApp(app)
+
+def test_home_page_has_list(testapp):
+    response = testapp.get("/", status=200)
+    inner_html = response.html
+
+
+# from webtest import TestApp
+
+# From learning_journal_basic import main
+
+# testapp = TestApp(min({}))
+
+# response = testapp.get("/", status=200)
+
+# from pprint import pprint
+
+# pprint(dir(response))
+
+# html = response.html
+
+# html.find_all("li")
+
+# len(html.find_all("li"))
+# >> 5
+
+# html.find("ul").child
+
+# <<ul = html.find('ul')
+# <<ul
+
+
+
 import unittest
 
 from pyramid import testing
