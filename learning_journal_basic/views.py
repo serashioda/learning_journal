5fd415c9026727a4bba5a5432e866b781016c5bd
Your Name <you@example.com>
2016-12-23 19:30:32 -0800

    updated dicionary

--- learning_journal_basic/views.py
+++ learning_journal_basic/views.py
@@ -1,30 +1,41 @@
 """."""
 from pyramid.view import view_config
+import io
 import os
 
 THIS_DIR = os.path.dirname(__file__)
 
 ENTRIES = [
-    {"id": 0, "title": "Week 1 Day 1", "creation_date": "Dec 22, 2016", "body": "<p>Apples are rotten</p>"},
-    {"id": 1, "title": "Week 1 Day 2", "creation_date": "Dec 23, 2016", "body": "<p>Oranges are rotten</p>"},
-    {"id": 2, "title": "Week 1 Day 3", "creation_date": "Dec 24, 2016", "body": "<p>Kiwis are rotten</p>"},
+    {"id": 0,
+        "title": "Week 1 Day 1",
+        "creation_date": "Dec 22, 2016",
+        "body": "Apples are rotten",
+    },
+    {"id": 1,
+        "title": "Week 1 Day 2",
+        "creation_date": "Dec 23, 2016",
+        "body": "Oranges are rotten",
+    },
+    {"id": 2,
+        "title": "Week 1 Day 3",
+        "creation_date": "Dec 24, 2016",
+        "body": "Kiwis are rotten",
+    },
 ]
 
 
-def get_entry(id):
-    """Get the entry from the dictionary by id"""
-    for entry in ENTRIES:
-        if entry["id"] == id:
-            return entry
-    return None
+# def get_entry(id):
+#     """Get the entry from the dictionary by id"""
+#     for entry in ENTRIES:
+#         if entry["id"] == id:
+#             return entry
+#     return None
 
 
 @view_config(route_name='list', renderer='templates/list.jinja2')
-def index_page(request):
+def list_page(request):
     """View for the home page."""
-    return {
-        "ENTRIES": ENTRIES,
-    }
+    return {"ENTRIES": ENTRIES}
 
 
 @view_config(route_name='detail', renderer='templates/detail.jinja2')
